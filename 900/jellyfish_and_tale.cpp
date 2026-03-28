c63d9795f015dd6d886dae5984e1a6219051f510
Ansh Shukla <anshshuklavg@gmail.com>
2026-03-28 11:53:31 +0530

    added with comment

--- 900/jellyfish_and_tale.cpp
+++ 900/jellyfish_and_tale.cpp
@@ -1,24 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n, a, b;
-        cin >> n >> a >> b;
+int main()
+{
+	int t;
+	cin >> t; // Read the number of test cases
+	while (t--)
+	{
+		long long a, b, n;
+		// Read the maximum timer value 'a', initial timer value 'b', and number of tools 'n'
+		cin >> a >> b >> n;
+		long long x[n];
+		// Read the increment values for each tool
+		for (int i = 0; i < n; i++)
+			cin >> x[i];
 
-        vector<int> arr(n);
-        for(int i = 0; i < n; i++){
-            cin >> arr[i];
-        }
+		// Initialize maximum_time with the initial timer value 'b'
+		long long maximum_time = b;
+		// Calculate the maximum time by adding the minimum of each tool's increment and (a-1)
+		for (int i = 0; i < n; i++)
+			maximum_time += min(x[i], a - 1);
 
-        long long maxitime = b;
-
-        for(int i = 0; i < n; i++){
-            maxitime += min(arr[i], a - 1);
-        }
-
-        cout << maxitime << endl;
-    }
+		// Output the maximum time until the bomb explodes
+		cout << maximum_time << endl;
+	}
+	return 0;
 }
+
+// Time Complexity (TC): O(n) = O(100)
+// Space Complexity (SC): O(n) = O(100)
